f018b7a271617978adf94e5e297bf5db8e30e16a
Tommination <thomas.rydow@gmail.com>
2026-01-28 17:07:16 +0100

    Altered loginDialog to extract username and password (instead of presenter doing it), made tests for loginDialog and added form validation of loginDialog (made username and password required, and indicates required fields with red if they are empty after user interaction)

--- frontend/components/ui/custom/login-dialog.tsx
+++ frontend/components/ui/custom/login-dialog.tsx
@@ -21,7 +21,7 @@ interface LoginDialogProps extends React.PropsWithChildren {
   passwordShown: boolean;
   dialogIsOpen: boolean;
   onEyeClick: () => void;
-  onLoginClick: React.FormEventHandler<HTMLFormElement>;
+  onLoginClick: (username: string, password: string) => void;
   onOpenChange: (value: boolean) => void;
 }
 
@@ -34,11 +34,20 @@ export function LoginDialog({
   onEyeClick,
   onLoginClick,
   onOpenChange,
-}: LoginDialogProps) {
+}: PopupDialogProps) {
+  function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+
+    onLoginClick(username, password);
+  }
   return (
     <Dialog open={dialogIsOpen} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[425px]">
-        <form onSubmit={onLoginClick}>
+        <form onSubmit={handleFormSubmit} role="form">
           <DialogHeader>
             <DialogTitle>Login</DialogTitle>
             <DialogDescription>
@@ -52,6 +61,8 @@ export function LoginDialog({
                 id="username"
                 name="username"
                 placeholder="Enter username..."
+                className="peer user-invalid:border-red-500"
+                required
               />
             </div>
             <div className="grid gap-3">
@@ -62,9 +73,11 @@ export function LoginDialog({
                   name="password"
                   type={passwordShown ? "text" : "password"}
                   placeholder="••••••••••"
-                  className=""
+                  className="peer user-invalid:border-red-500"
+                  required
                 />
                 <button
+                  aria-label={passwordShown ? "hide password" : "show password"}
                   className="absolute w-5 top-2 right-2"
                   onClick={onEyeClick}
                 >
@@ -74,6 +87,9 @@ export function LoginDialog({
                     <Eye size={20} className="text-muted-foreground" />
                   )}
                 </button>
+                <p className="text-sm text-red-500 hidden peer-user-invalid:block">
+                  Required
+                </p>
               </div>
             </div>
           </div>
@@ -88,7 +104,12 @@ export function LoginDialog({
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <Button type="submit">Login</Button>
+            <Button
+              type="submit"
+              className="group-[&:has(:invalid)]:pointer-events-none group-[&:has(:invalid)]:opacity-50"
+            >
+              Login
+            </Button>
           </DialogFooter>
         </form>
       </DialogContent>

beaeb4cb692c193d1005b1c2cf59a160d7c413cb
Daniel Olsson <160970261+OlofDaniel@users.noreply.github.com>
2026-01-28 13:24:01 +0100

    feat/fix: added recruiter button that links to /recruiter and changed login and signup buttons to more clean version

--- frontend/views/header-view.tsx
+++ frontend/views/header-view.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { User, UserPlus } from "lucide-react";
+import { User, UserPlus, ShieldUser } from "lucide-react";
 import Link from "next/link";
 
 interface headerViewProps {
@@ -9,17 +9,24 @@ interface headerViewProps {
 export function HeaderView({ onLoginClick }: headerViewProps) {
   return (
     <div className="grid grid-cols-3 items-center bg-neutral-200 py-5">
-      <span></span>
+      <span className="ml-3">
+        <Link href="/recruiter">
+          <Button className="w-25" variant="link">
+            <ShieldUser />
+            Recruiter
+          </Button>
+        </Link>
+      </span>
       <span className="justify-self-center text-5xl font-serif">
         Amusement Park
       </span>
-      <div className="flex justify-self-end gap-5 mr-5">
-        <Button className="w-25" onClick={onLoginClick}>
+      <div className="flex justify-self-end mr-5">
+        <Button variant="link" className="w-25" onClick={onLoginClick}>
           <User />
           Login
         </Button>
         <Link href="/signup">
-          <Button className="w-25">
+          <Button className="w-25" variant="link">
             <UserPlus />
             Sign Up
           </Button>

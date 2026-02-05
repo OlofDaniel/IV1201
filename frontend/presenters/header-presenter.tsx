b499079745bc3daf28b109068f8d5671c81c2df8
Tommination <thomas.rydow@gmail.com>
2026-02-05 12:12:58 +0100

    fix: added regex to comment explaining frontend form validation

--- frontend/presenters/header-presenter.tsx
+++ frontend/presenters/header-presenter.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { HeaderView } from "@/views/header-view";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/lib/Redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/lib/Redux/store";
 import { setDialogIsOpen } from "@/models/Redux/login-slice";
 
 export function HeaderPresenter() {
   const dispatch = useDispatch<AppDispatch>();
-
+  const { loggedIn } = useSelector((state: RootState) => state.auth);
   const onLoginClick = () => {
     dispatch(setDialogIsOpen(true));
   };
-  return <HeaderView onLoginClick={onLoginClick} />;
+  return <HeaderView onLoginClick={onLoginClick} loggedIn={loggedIn} />;
 }

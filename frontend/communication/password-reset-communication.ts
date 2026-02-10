4e18573f8b11cd775c54a028fad0ea90b162ff91
Thomas Rydow <thomas.rydow@gmail.com>
2026-02-10 13:26:56 +0100

    fix: changed reset password communication comments and private method names (had been copied from login communication)

--- frontend/communication/password-reset-communication.ts
+++ frontend/communication/password-reset-communication.ts
@@ -4,12 +4,12 @@ type passwordresetPayload = {
   email: string;
 };
 /*
-  Communication logic for the login process:
-  postLogin: handles the asynchronous communication from the frontend to the backend with the users credentials
-  response: saves the response from the fetch call to the login endpoint after the HTTP POST request
+  Communication logic for the password reset process:
+  postReset: handles the asynchronous communication from the frontend to the backend with the users email
+  response: saves the response from the fetch call to the reset endpoint after the HTTP POST request
   JSON.stringify: formats the users credentials as a JSON payload for authentication
 */
-const postLogin = async (payload: passwordresetPayload) => {
+const postReset = async (payload: passwordresetPayload) => {
   const response = await fetch("http://127.0.0.1:8000/reset", {
     method: "POST",
     headers: {
@@ -26,9 +26,9 @@ const postLogin = async (payload: passwordresetPayload) => {
 };
 
 /*
-  Asynchronous thunk for the login process:
-  postLoginThunk: dispatches status updates (pending, fulfilled, rejected) of the login loading state
-  await postLogin: executes the API call with the login payload and waits for a response
+  Asynchronous thunk for the reset process:
+  postResetThunk: dispatches status updates (pending, fulfilled, rejected) of the password reset loading state
+  await postReset: executes the API call with the reset payload and waits for a response
   rejectWithValue: returns the correct error message if an error occurs
 */
 export const postResetThunk = createAsyncThunk<
@@ -37,7 +37,7 @@ export const postResetThunk = createAsyncThunk<
   { rejectValue: string }
 >("reset/postResetThunk", async (payload, thunkAPI) => {
   try {
-    await postLogin(payload);
+    await postReset(payload);
   } catch (error) {
     if (error instanceof Error) {
       return thunkAPI.rejectWithValue(error.message);
